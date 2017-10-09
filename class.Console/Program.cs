9844c608357175d4b90f75d312cb6fddeb08e46f
Véronique Zimmermann <zimmermannveronique@protonmail.com>
2017-10-09 13:34:08 +0200

    Konsolenfarbe
    Spaß mit Farben

--- class.Console/Program.cs
+++ class.Console/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
+using static System.ConsoleColor;
 
-namespace @class.Console
+namespace classConsole_ConsoleApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ForegroundColor = Cyan;
+            BackgroundColor = DarkMagenta; // bezieht sich nur auf Text
+            WriteLine("The current window width is {0} Chars", Console.WindowWidth);
+            ReadLine();
+            SetWindowSize(100, 40);
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("The current window width is {0} Chars", Console.WindowWidth);
+            Console.ReadLine();
+
         }
     }
 }
